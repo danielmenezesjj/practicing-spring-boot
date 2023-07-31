5bc97c3067c4f7bd2b3de074171f40cde53813b1
usuario <danielmenezes2512@gmail.com>
2023-07-30 23:20:47 -0300

    modelando o banco de dados

---
+++ src/main/resources/db/migration/V1__create-table-pacientes.sql
@@ -0,0 +1,10 @@
+create table pacientes(
+                          id_paciente INT AUTO_INCREMENT PRIMARY KEY,
+                          nome VARCHAR (255) NOT NULL,
+                          cpf varchar(255) not null,
+                          dt_nascimento varchar(255) not null,
+                          rg varchar(255) not null,
+                          naturalidade varchar(255) not null,
+                          celular varchar(255) not null,
+                          email varchar(255) not null
+);
